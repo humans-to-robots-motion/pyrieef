f87cbd978a445cf566286f7785808542157014c1
Jim <mainprice@gmail.com>
2018-08-23 12:51:46 +0200

    work on dijkstra

--- src/graph/shortest_path.py
+++ src/graph/shortest_path.py
@@ -7,10 +7,23 @@ def symmetrize(a):
     return a + a.T - np.diag(a.diagonal())
 
 
-def costmap_to_graph(cost):
-    """ Converts a costmap to a compressed sparse graph 
+def costmap_node_id(cost, i, j):
+    return i + j * cost.shape[0]
 
+def eight_connected_neiborghs(i, j):
+    coord 
+
+def costmap_to_graph(cost):
+    """ Converts a costmap to a compressed sparse graph
+
+        cost : The M x N matrix of costs. cost[i,j] 
+               gives the cost of a certain node
+        node_map_coord  = (i, j)
+        node_graph_id   = i + j * M
     """
 
     nb_nodes = cost.shape[0] * cost.shape[1]
     graph_dense = np.zeros((nb_nodes, nb_nodes))
+
+    for (i, j), c_ij in np.ndenumerate(cost):
+
