1ce94ec22c75c149753195f0b07d361807a60739
Jim Mainprice <mainprice@gmail.com>
2020-02-15 11:01:33 +0100

    Fix displaying

--- examples/plot_value_iteration.py
+++ examples/plot_value_iteration.py
@@ -29,26 +29,29 @@ import itertools
 
 
 # Creates a workspace with just one circle
+nb_points = 30
 workspace = Workspace()
-workspace.obstacles = [Circle(origin=[.0, .0], radius=0.1)]
+workspace.obstacles = [Circle(origin=[.1, .2], radius=0.1)]
 renderer = WorkspaceDrawer(workspace)
 sdf = SignedDistanceWorkspaceMap(workspace)
-occupany = occupancy_map(20, workspace)
-reward = np.where(occupany > 0, None, -.001)
-reward[0, 0] = 100
+occupany = occupancy_map(nb_points, workspace)
+reward = np.where(occupany > 0, -10., -.001)
+reward[0, 0] = 10
 mdp = GridMDP(reward.tolist(), terminals=[(0, 0)])
 X = value_iteration(mdp)
 value = np.zeros(reward.shape)
 for x in X:
     value[x] = X[x]
+value = np.flip(value, 1).T
 
 # Creates a vector field as the gradient of the signed distance field
-nb_points = 14
-X, Y = workspace.box.meshgrid(nb_points)
+grid = workspace.pixel_map(nb_points)
+f = RegressedPixelGridSpline(value, grid.resolution, grid.extent)
+X, Y = workspace.box.meshgrid(20)
 U, V = np.zeros(X.shape), np.zeros(Y.shape)
 for i, j in itertools.product(range(X.shape[0]), range(X.shape[1])):
     p = np.array([X[i, j], Y[i, j]])
-    g = sdf.gradient(p)
+    g = f.gradient(p)
     g /= np.linalg.norm(g)
     U[i, j] = g[0]
     V[i, j] = g[1]
@@ -56,8 +59,6 @@ for i, j in itertools.product(range(X.shape[0]), range(X.shape[1])):
 renderer.set_drawing_axis(i)
 renderer.draw_ws_obstacles()
 renderer.draw_ws_point([0, 0], color='r', shape='o')
-renderer.background_matrix_eval = True
-# renderer.draw_ws_background(sdf, color_style=plt.cm.Blues)
-renderer.draw_ws_img(value, interpolate="bicubic", color_style=plt.cm.Blues)
+renderer.draw_ws_img(value, interpolate="none", color_style=plt.cm.Blues)
 renderer._ax.quiver(X, Y, U, V, units='width', color='k')
 renderer.show()
