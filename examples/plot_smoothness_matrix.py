33c9e404c62891acb00de766d0a7f7aeb45563dd
Jim Mainprice <mainprice@gmail.com>
2020-09-09 16:39:47 +0200

    Add sample

--- examples/plot_smoothness_matrix.py
+++ examples/plot_smoothness_matrix.py
@@ -21,11 +21,13 @@ import demos_common_imports
 from pyrieef.motion.objective import *
 import matplotlib.pyplot as plt
 
+SAMPLES = True
+
 dim = 1
 trajectory = linear_interpolation_trajectory(
     q_init=np.zeros(dim),
     q_goal=np.ones(dim),
-    T=10
+    T=100
 )
 objective = MotionOptimization2DCostMap(
     T=trajectory.T(),
@@ -41,17 +43,30 @@ H1 = objective.objective.hessian(trajectory.active_segment())
 np.set_printoptions(suppress=True, linewidth=200, precision=0,
                     formatter={'float_kind': '{:2.0f}'.format})
 print(H1.shape)
-print(H1/200000)
+print(H1 / 200000)
 
 H2 = objective.create_smoothness_metric() / 10000
 # H2 = np.loadtxt("tmp.txt")
 np.set_printoptions(suppress=True, linewidth=200, precision=0,
                     formatter={'float_kind': '{:2.0f}'.format})
 
+if SAMPLES:
+    # Here plot te sample
+    std_dev = 1.
+    nb_samples = 20
+    # WARNING: This coefficent has to be large
+    H1[0, 0] = 1000
+    cov = np.linalg.inv(H1)
+    mean = np.array([0] * cov.shape[0])
+    samples = np.random.multivariate_normal(mean, cov, nb_samples).T
+    samples = std_dev * samples
+    plt.plot(samples)
+else:
+    # Here plot the matrix collumns
+    print(H1)
+    A_inv = np.linalg.inv(H1)
+    print(np.max(A_inv))
+    A_inv /= np.max(A_inv)
+    plt.plot(A_inv)
 
-print(H1)
-A_inv = np.linalg.inv(H1)
-print(np.max(A_inv))
-A_inv /= np.max(A_inv)
-plt.plot(A_inv)
 plt.show()
