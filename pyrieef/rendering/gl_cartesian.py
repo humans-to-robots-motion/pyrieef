e28ec8baccb9b1a8b95545b3a5c0dae47416fb8e
Jim Mainprice <mainprice@gmail.com>
2021-04-21 20:58:38 +0200

    Make cartesian display work

--- pyrieef/rendering/gl_cartesian.py
+++ pyrieef/rendering/gl_cartesian.py
@@ -1,16 +1,38 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2021, University of Stuttgart
+# All rights reserved.
+#
+# Permission to use, copy, modify, and distribute this software for any purpose
+# with or without   fee is hereby granted, provided   that the above  copyright
+# notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO THIS  SOFTWARE INCLUDING ALL  IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS. IN NO EVENT SHALL THE AUTHOR  BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER RESULTING  FROM
+# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
+# PERFORMANCE OF THIS SOFTWARE.
+#
+#                                        Jim Mainprice on Wed April 21 2021
+
+
 import open3d as o3d
 
 print("Let's define some primitives")
-mesh_box = o3d.geometry.TriangleMesh.create_box(width=1.0,
-                                                height=1.0,
-                                                depth=1.0)
+mesh_box = o3d.geometry.TriangleMesh.create_box(
+    width=1.0, height=1.0, depth=1.0)
 mesh_box.compute_vertex_normals()
 mesh_box.paint_uniform_color([0.9, 0.1, 0.1])
-mesh_sphere = o3d.geometry.TriangleMesh.create_sphere(radius=1.0)
+mesh_sphere = o3d.geometry.TriangleMesh.create_sphere(
+    radius=1.0)
+
 mesh_sphere.compute_vertex_normals()
 mesh_sphere.paint_uniform_color([0.1, 0.1, 0.7])
-mesh_cylinder = o3d.geometry.TriangleMesh.create_cylinder(radius=0.3,
-                                                          height=4.0)
+mesh_cylinder = o3d.geometry.TriangleMesh.create_cylinder(
+    radius=0.3, height=4.0)
+
 mesh_cylinder.compute_vertex_normals()
 mesh_cylinder.paint_uniform_color([0.1, 0.9, 0.1])
 mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
