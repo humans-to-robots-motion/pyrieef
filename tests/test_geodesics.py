43d88b11f4d1a452eae242827db28e9f7e68e3d1
Jim Mainprice <mainprice@gmail.com>
2021-10-28 10:25:39 +0200

    add test misc

--- tests/test_geodesics.py
+++ tests/test_geodesics.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2018, University of Stuttgart
+# Copyright (c) 2018
 # All rights reserved.
 #
 # Permission to use, copy, modify, and distribute this software for any purpose
