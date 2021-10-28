Fix gradient operator

pyrieef/geometry/heat_diffusion.py
@@ -173,30 +173,35 @@ def crank_nicholson_2d(dt, h, source_grid, iterations, occupancy):
     return costs
 
 
-def discrete_2d_gradient(M, N, axis=0):
+def discrete_2d_gradient(M, N, dx=1., axis=0):
     """
     Efficient allocation of the Discrete-2D-Gradient
     """
     dim = M * N
     if axis == 0:
-        A = np.identity(dim)
-        A[range(dim - 1), range(1, dim)] = -1
+        A = np.zeros((dim, dim))
+        np.fill_diagonal(A, -1)
 
-        # every end of row left difference
-        A[range(M - 1, M * (N - 1), M), range(M, dim, M)] = 0
-        A[range(M - 1, dim, M), range(M - 1, dim, M)] = -1
-        A[range(M - 1, dim, M), range(M - 2, dim, M)] = 1
-
-    if axis == 1:
-        A = np.identity(dim)
-        A[range(M * (N - 1)), range(M, dim)] = -1
+        A[range(M * (N - 1)), range(M, dim)] = 1
 
         # every end of collumn left difference
         last_block = range(M * (N - 1), dim)
-        A[last_block, last_block] = -1
-        A[last_block, range(M * (N - 2), M * (N - 1))] = 1
+        A[last_block, last_block] = 1
+        A[last_block, range(M * (N - 2), M * (N - 1))] = -1
 
-    return A
+    if axis == 1:
+        
+        A = np.zeros((dim, dim))
+        np.fill_diagonal(A, -1)
+
+        A[range(dim - 1), range(1, dim)] = 1
+
+        # every end of row left difference
+        A[range(M - 1, M * (N - 1), M), range(M, dim, M)] = 0
+        A[range(M - 1, dim, M), range(M - 1, dim, M)] = 1
+        A[range(M - 1, dim, M), range(M - 2, dim, M)] = -1
+
+    return  (1/dx)  * A
 
 
 def discrete_2d_laplacian(M, N, matrix_form=False):
@@ -261,18 +266,22 @@ def divergence(f):
 def distance_from_gradient(U, V, dh, f=None):
     """
     Get a scalar field from gradients
+
+    TODO: 
+        - Check wether the axis actually corresponds to Dx and Dy..
+        - This depends on what we excpect for U and V
+        - It could be that since this is an ill-posed problem it does
+        not matter..
+        - When we flip the sign of Dx, Dy the solition does not seem to change
     """
     N = U.shape[0]
-    Dx = (1. / dh) * discrete_2d_gradient(N, N, axis=0)
-    Dy = (1. / dh) * discrete_2d_gradient(N, N, axis=1)
-    grad = np.hstack([U.flatten(), V.flatten()])
+    Dx = discrete_2d_gradient(N, N, dx=dh, axis=0)
+    Dy = discrete_2d_gradient(N, N, dx=dh, axis=1)
     D = np.vstack([Dx, Dy])
+    grad = np.hstack([U.flatten(), V.flatten()])
     if f is not None:
         grad = np.dot(D, f.flatten())
     phi = np.dot(np.linalg.pinv(D), grad)
-    print(grad.shape)
-    print(D.shape)
-    print(phi.shape)
     d = np.linalg.norm(grad - np.dot(D, phi))
     print("d : ", d)
     phi.shape = (N, N)

pyrieef/kinematics/robot.py
@@ -189,14 +189,21 @@ def create_keypoints(nb_keypoints, segments):
     for s in (segments):
         length += s.length()
     keypoints = []
-    dl = length / nb_keypoints
-    for s in segments:
-        k = math.floor(s.length() / dl)
-        d_alpha = (1. / float(k))
-        d = 0.
-        for i in range(int(k)):
-            keypoints.append(d * s.p1() + (1 - d) * s.p2())
-            d += d_alpha
+    dl = length / (nb_keypoints - 1)
+    d_rest = 0
+    print(dl)
+    for k, s in enumerate(segments):
+        d = d_rest
+        while s.length() - d > 1e-6:
+            alpha = d / s.length()
+            p = (1 - alpha) * s.p1() + alpha * s.p2()
+            keypoints.append(p)
+            if len(keypoints) == nb_keypoints :
+                return keypoints
+            d += dl
+        d_rest = d - s.length()
+        if k == len(segments) -1:
+            keypoints.append(s.p2())
     return keypoints
 
 
@@ -224,13 +231,15 @@ def create_freeflyer_from_segments(
             "nb_keypoints"] if nb_keypoints is None else nb_keypoints
         points = config["segments"]
         n = 2 if config["planar"] else 3  # dimensionality of the FF
+        all_points = sorted(points.items())
+        print(all_points)
         segments = [Segment(
             p1=np.array(p[0:n]),
-            p2=np.array(p[n:2 * n])) for k, p in sorted(points.items())]
+            p2=np.array(p[n:2 * n])) for k, p in all_points]
         keypoints = create_keypoints(nb_keypoints, segments)
         keypoints_dic = {}
         for i, point in enumerate(keypoints):
-            keypoints_dic["s" + str(i)] = point
+            keypoints_dic["s{:03}".format(i)] = point
         robot = Freeflyer(
             config["name"],
             keypoints_dic,

tests/test_geodesics.py
@@ -158,21 +158,24 @@ def test_gradient_1d_operator():
 
 def test_gradient_operator():
     """
-    TODO test the function here..
-    finish this
+    This function tests the linear gradient operator
+
+    Note: that we have small deviations from the numpy implementations
+          We should check that these deviations come from the boundry
+          conditions. The numpy version has some different things happening.
     """
 
     N = 10
-    Dx = discrete_2d_gradient(N, N, axis=0)
-    Dy = discrete_2d_gradient(N, N, axis=1)
-    D = np.vstack([Dx, Dy])
 
-    f = PolynomeTestFunction()
     x = y = np.linspace(0, 1, N)
-    X, Y = np.meshgrid(x, y)
-    Z = f(np.stack([X, Y]))
+    Q = np.stack((np.meshgrid(x, y)))
+    Z = PolynomeTestFunction().forward(Q)
+    # Z = LinearTestFunction().forward(Q)
 
-    grad1 = np.dot(D, Z.flatten())
+    grad1 = np.dot(np.vstack([
+        discrete_2d_gradient(N, N, axis=0),
+        discrete_2d_gradient(N, N, axis=1)]), Z.flatten())
+
     grad2 = np.stack([
         np.gradient(Z, axis=0).flatten(),
         np.gradient(Z, axis=1).flatten()]).flatten()