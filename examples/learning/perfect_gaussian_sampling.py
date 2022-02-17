f39b54d9a98288f1bc5b8284b70983a09f3d4197
Jim Mainprice <mainprice@gmail.com>
2022-02-17 22:20:42 +0100

    Fix script

--- examples/learning/perfect_gaussian_sampling.py
+++ examples/learning/perfect_gaussian_sampling.py
@@ -15,7 +15,7 @@
 # OTHER TORTIOUS ACTION,   ARISING OUT OF OR IN    CONNECTION WITH THE USE   OR
 # PERFORMANCE OF THIS SOFTWARE.
 #
-#                                    Jim Mainprice on Thursday February 13 2022
+#                                         Jim Mainprice on Thursday Feb 17 2022
 
 import demos_common_imports
 
@@ -29,10 +29,14 @@ from matplotlib.patches import Ellipse
 import matplotlib.transforms as transforms
 from numpy.testing import assert_allclose
 
+# optimizer module
+from scipy.optimize import minimize, BFGS, Bounds, NonlinearConstraint
 
-def confidence_ellipse(ax,
-                       cov=None, x=None, y=None,
-                       n_std=3.0, facecolor='none', **kwargs):
+
+def confidence_ellipse(
+        ax,
+        cov=None, x=None, y=None,
+        n_std=3.0, facecolor='none', **kwargs):
     """
     Create a plot of the covariance confidence ellipse of *x* and *y*.
 
@@ -95,11 +99,17 @@ def confidence_ellipse(ax,
     return ax.add_patch(ellipse)
 
 
-def sample_data():
+def sample_data(N):
+    """
+    Samples from a zero mean 2D multivariate_normal
+
+    Parameters
+    ----------
+    N : int
+        The number of sampled particules
+    """
 
     np.random.seed(4)
-
-    nb_samples = 10
 
     # mean
     mu = np.zeros(2)
@@ -109,7 +119,7 @@ def sample_data():
     A = np.array([[1., .1], [.1, .2]])
     Sigma = A.transpose() @ A
 
-    M = np.random.multivariate_normal(mu, Sigma, nb_samples).T
+    M = np.random.multivariate_normal(mu, Sigma, N).T
 
     return (Sigma, M)
 
@@ -122,34 +132,43 @@ class EmpiricalCovariance(DifferentiableMap):
         self._n = n             # nb of random variables
 
     def output_dimension(self):
-        return 1
+        return self._n * self._n
 
     def input_dimension(self):
-        return n
+        return self._m * self._n
+
+    def jacobian(self, q):
+        """ Should return a matrix or single value of
+                m x n : ouput x input (dimensions)
+            by default the method returns the finite difference jacobian.
+            WARNING the object returned by this function is a numpy matrix."""
+        return np.array(finite_difference_jacobian(self, q)).reshape(
+            self.output_dimension(), self.input_dimension())
 
     def forward(self, x):
+        assert x.size == self.input_dimension()
         X = x.reshape(self._m, self._n)
-        print("M0 (2) : ", X)
         covariance = X.T @ X
         covariance /= float(self._m - 1)
-        print("M0 shape : ", X.shape)
-        print("covariance shape : ", covariance.shape)
-        print("shape : ", covariance.shape)
         return covariance.flatten()
 
 
 def cov_own(X):
     """
-    Computes covariance using a simple formula
+    Computes covariance using a simple formular
+    for the two 2D case
     """
     Sigma = np.zeros((X.shape[1], X.shape[1]))
     for x in X:
         Sigma += np.outer(x, x)
-    Sigma /= float(X.shape[0] - 1)
-    return Sigma
+    return Sigma / float(X.shape[0] - 1)
+
+# ------------------------------------------------------------------------------
+#             Generate Test Data
+# ------------------------------------------------------------------------------
 
 
-(Sigma, M) = sample_data()
+(Sigma, M) = sample_data(N=100)
 
 print(M.shape)
 
@@ -166,32 +185,72 @@ y = y - y0
 
 M0 = np.array([x, y]).T
 
-print("M0 shape : ", M0.shape)
-print("M0 (1) : ", M0)
+# print("M0 shape : ", M0.shape)
+# print("M0 (1) : ", M0)
 
 cov1 = np.cov(M0.T, bias=False)
 cov2 = EmpiricalCovariance(x.size, 2)(M0.flatten()).reshape(2, 2)
 cov3 = cov_own(M0)
 
+# print("mean0 (x) : ", np.mean(M0[0, :]))
+# print("mean0 (y) : ", np.mean(M0[1, :]))
 
-print("mean0 (x) : ", np.mean(M0[0, :]))
-print("mean0 (y) : ", np.mean(M0[1, :]))
-
-print("cov1 : ", cov1)
-print("cov2 : ", cov2)
-print("cov3 : ", cov3)
+# print("cov1 : ", cov1)
+# print("cov2 : ", cov2)
+# print("cov3 : ", cov3)
 
 assert_allclose(cov1.flatten(), cov2.flatten())
 
+# ------------------------------------------------------------------------------
+#             OPTIMIZE
+# ------------------------------------------------------------------------------
 
-# ax1.plot(x, y, 'x', c='r')
-# confidence_ellipse(ax1, cov=Sigma, edgecolor='red')
-# confidence_ellipse(ax1, x=x, y=y, edgecolor='blue')
+#           min_x | M - M_0 |^2
+#
+#               s.t.  | M^T M - Sigma |^2 = 0
+
+# Objective is to stay close to the original empirical distribition
+f = SquaredNorm(M0.flatten())
+
+# The equality constraint is to match the second moment
+# i.e., here the convariance matrix
+empirical_covariance = EmpiricalCovariance(M0.shape[0], M0.shape[1])
+h = Compose(SquaredNorm(Sigma.flatten()), empirical_covariance)
+nonlinear_constraint = NonlinearConstraint(
+    h.forward,
+    0,
+    0,
+    jac=h.gradient,
+    hess=BFGS())
+
+res = minimize(
+    f.forward,
+    M0.flatten(),
+    method='trust-constr',
+    jac=f.gradient,
+    hess=f.hessian,
+    constraints=[nonlinear_constraint],
+    options={'verbose': 1})
+
+M1 = res.x.reshape(M0.shape)
+print(M0.flatten())
+print(M1.flatten())
+
+print("- np.cov(M0) : ", np.cov(M0.T, bias=False))
+print("- np.cov(M1) : ", np.cov(M1.T, bias=False))
+print("- Sigma : ", Sigma)
+
+ax1.plot(M0[:, 0], M0[:, 1], 'x', c='r')
+confidence_ellipse(ax1, x=x, y=y, edgecolor='r')
+confidence_ellipse(ax1, cov=Sigma, edgecolor='k')
 # ax1.axis('equal')
-# ax1.set_title('Original Samples')
+ax1.set(xlim=(-3, 3), ylim=(-1.5, 1.5))
+ax1.set_title('Original Samples')
 
-# ax2.plot(x, y, 'x', c='b')
-# ax2.axis('equal')
-# ax2.set_title('Optmized Samples')
+ax2.plot(M1[:, 0], M1[:, 1], 'x', c='b')
+confidence_ellipse(ax2, x=M1[:, 0], y=M1[:, 1], edgecolor='b', linewidth=5)
+confidence_ellipse(ax2, cov=Sigma, edgecolor='k')
+ax2.set(xlim=(-3, 3), ylim=(-1.5, 1.5))
+ax2.set_title('Optmized Samples')
 
-# plt.show()
+plt.show()
